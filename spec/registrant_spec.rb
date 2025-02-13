10aa74ec6404c6ac700d67d3145cbac53d8e45fb
Katya Weicht <katya.weicht@gmail.com>
2025-02-12 21:44:04 -0700

    Finish iteration one

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -20,5 +20,22 @@ RSpec.describe Registrant do
             expect(@registrant_1.age).to eq(18)
             expect(@registrant_2.age).to eq(15)
         end
+
+        it 'does or does not have a permit' do
+            expect(@registrant_1.permit?).to eq(true)
+            expect(@registrant_2.permit?).to eq(false)
+        end
+    end
+
+    describe '#other data and actions' do
+        it 'has license_data' do
+            expect(@registrant_1.license_data).to eq({:written => false, :license => false, :renewed => false})
+            expect(@registrant_1.license_data).to eq({:written => false, :license => false, :renewed => false})
+        end
+
+        it 'can earn a permit' do
+            @registrant_2.earn_permit
+            expect(@registrant_2.permit?).to eq(true)
+        end
     end
 end
