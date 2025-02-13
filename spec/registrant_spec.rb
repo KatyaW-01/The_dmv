5c75af7962f0a3b1b670ce2584343fb6a5e0ffe3
Katya Weicht <katya.weicht@gmail.com>
2025-02-12 21:07:51 -0700

    Add initialize method to registrant class

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -1,5 +1,20 @@
 require 'spec_helper'
 
-Rspec.describe Registrant do
+RSpec.describe Registrant do
     before(:each) do
+        @registrant_1 = Registrant.new('Bruce', 18, true )
+        @registrant_2 = Registrant.new('Penny', 15 )
+    end
+
+    describe '#initialize' do
+        it 'exists' do
+            expect(@registrant_1).to be_an_instance_of(Registrant)
+            expect(@registrant_2).to be_an_instance_of(Registrant)
+        end
+
+        it 'has a name' do
+            expect(@registrant_1.name).to eq('Bruce')
+            expect(@registrant_2.name).to eq('Penny')
+        end
+    end
 end
