89fc4f39db9b283c4e7718e900029de10078a71a
Katya Weicht <katya.weicht@gmail.com>
2025-02-13 17:09:54 -0700

    Add plate_type variable

--- lib/facility.rb
+++ lib/facility.rb
@@ -1,7 +1,7 @@
 require 'date'
 require './lib/vehicle'
 class Facility
-  attr_reader :name, :address, :phone, :services, :registered_vehicles
+  attr_reader :name, :address, :phone, :services, :registered_vehicles, :plate_type
 
   def initialize(facility_data)
     @name = facility_data[:name]
@@ -9,6 +9,7 @@ class Facility
     @phone = facility_data[:phone]
     @services = []
     @registered_vehicles = []
+    @plate_type = plate_type 
   end
 
   def add_service(service)
@@ -17,13 +18,16 @@ class Facility
 
   def collected_fees
     fees = 0
-     
+    #if statement to collect fees based on plate type 
   end
 
 
   def register_vehicle(vehicle)
     vehicle.registration_date = Date.today
+    #set plate type here
     @registered_vehicles << vehicle
+
+
 
   end
   
