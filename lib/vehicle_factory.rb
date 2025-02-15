6e775b573e4f6e9f1f9796a7d26888da1d141efc
Katya Weicht <katya.weicht@gmail.com>
2025-02-15 13:54:03 -0700

    Write tests for VehicleFactory class

--- lib/vehicle_factory.rb
+++ lib/vehicle_factory.rb
@@ -1,3 +1,12 @@
+require './lib/vehicle'
 class VehicleFactory
-    
+    def initialize
+
+    end
+
+    def create_vehicles(vehicles)
+      
+        
+
+    end
 end
--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -1,2 +1,19 @@
 require './lib/dmv_data_service'
 require 'spec_helper'
+require 'pry'
+
+RSpec.describe VehicleFactory do
+    it 'exists' do
+        factory = VehicleFactory.new
+        
+        expect(factory).to be_an_instance_of(VehicleFactory)
+    end
+
+    it 'can create vehicles' do
+        factory = VehicleFactory.new
+        binding.pry
+        wa_ev_registrations = DmvDataService.new.wa_ev_registrations
+        factory.create_vehicles(wa_ev_registrations)
+
+    end
+end
