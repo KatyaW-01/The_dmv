57e3e499340b1c339102253b932ceafb2a4bdc7f
Katya Weicht <katya.weicht@gmail.com>
2025-02-16 00:12:25 -0700

    Update tests for ny dmv office

--- lib/facility_objects.rb
+++ lib/facility_objects.rb
@@ -6,9 +6,11 @@ class FacilityObjects
 
     def create_facilities(facilities)
         facilities.map do |facility|
-            new_facility = Facility.new({name: facility[:dmv_office] || facility[:office_name], 
-                                        address: [facility[:address_li],facility[:address__1],facility[:city],facility[:state],facility[:zip]].join(" "), 
-                                        phone: facility[:phone] })
+            new_facility = Facility.new({
+                name: facility[:dmv_office] || facility[:office_name], 
+                address: [facility[:address_li],facility[:address__1],facility[:city],facility[:state],facility[:zip]].join(" ") || [facility[:street_address_line_1],facility[:city],facility[:state],facility[:zip_code]].join(" "), 
+                phone: facility[:phone] || facility[:public_phone_number] 
+                                    })
             #binding.pry
         end
     end
--- spec/facility_objects_spec.rb
+++ spec/facility_objects_spec.rb
@@ -32,12 +32,27 @@ RSpec.describe FacilityObjects do
         end
 
         it 'can create facilities at multiple locations' do
+            co_facilities = DmvDataService.new.co_dmv_office_locations
+
+            facilities = @facility.create_facilities(co_facilities)
+
+            expect(facilities).to be_an_instance_of(Array)
+
+            expect(facilities[0]).to be_an_instance_of(Facility)
+            expect(facilities[0].name).to eq("DMV Tremont Branch")
+            expect(facilities[0].address).to eq('2855 Tremont Place Suite 118 Denver CO 80205')
+            expect(facilities[0].phone).to eq("(720) 865-4600")
+            expect(facilities[0].services).to eq([])
+
             new_york_facilities = DmvDataService.new.ny_dmv_office_locations
 
             facilities = @facility.create_facilities(new_york_facilities)
 
             expect(facilities[0]).to be_an_instance_of(Facility)
             expect(facilities[0].name).to eq("LAKE PLACID")
+            #expect(facilities[0].address).to eq("2693 MAIN STREET LAKE PLACID NY 12946")
+            expect(facilities[0].phone).to eq(nil)
+            expect(facilities[0].services).to eq([])
 
 
 
