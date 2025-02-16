bb0c95ad7d6ae03cef92c492aace6cc6f10e342b
Katya Weicht <katya.weicht@gmail.com>
2025-02-15 21:37:53 -0700

    Modify create_facilities method to accomodate ny offices

--- lib/facility_objects.rb
+++ lib/facility_objects.rb
@@ -6,8 +6,10 @@ class FacilityObjects
 
     def create_facilities(facilities)
         facilities.map do |facility|
-            new_facility = Facility.new({name: facility[:dmv_office], address: [facility[:address_li],facility[:address__1],facility[:city],facility[:state],facility[:zip]].join(" ") , phone: facility[:phone] })
-            # binding.pry
+            new_facility = Facility.new({name: facility[:dmv_office] || facility[:office_name], 
+                                        address: [facility[:address_li],facility[:address__1],facility[:city],facility[:state],facility[:zip]].join(" "), 
+                                        phone: facility[:phone] })
+            #binding.pry
         end
     end
 end
--- spec/facility_objects_spec.rb
+++ spec/facility_objects_spec.rb
@@ -23,6 +23,24 @@ RSpec.describe FacilityObjects do
             expect(facilities[0].address).to eq('2855 Tremont Place Suite 118 Denver CO 80205')
             expect(facilities[0].phone).to eq("(720) 865-4600")
             expect(facilities[0].services).to eq([])
+
+            expect(facilities[1]).to be_an_instance_of(Facility)
+            expect(facilities[1].name).to eq("DMV Northeast Branch")
+            expect(facilities[1].address).to eq("4685 Peoria Street Suite 101 Denver CO 80239")
+            expect(facilities[1].phone).to eq("(720) 865-4600")
+            expect(facilities[1].services).to eq([])
+        end
+
+        it 'can create facilities at multiple locations' do
+            new_york_facilities = DmvDataService.new.ny_dmv_office_locations
+
+            facilities = @facility.create_facilities(new_york_facilities)
+
+            expect(facilities[0]).to be_an_instance_of(Facility)
+            expect(facilities[0].name).to eq("LAKE PLACID")
+
+
+
         end
     end
 end
