e96fba33b59618705a918abb63d14a50cfa03f85
Katya Weicht <katya.weicht@gmail.com>
2025-02-15 19:49:04 -0700

    Test for specific instances of vehicles

--- lib/vehicle_factory.rb
+++ lib/vehicle_factory.rb
@@ -7,7 +7,8 @@ class VehicleFactory
 
     def create_vehicles(vehicle_registration)
         vehicle_registration.map do |registration|
-            new_vehicle = Vehicle.new({vin: registration[:vin_1_10], year: registration[:model_year], make: registration[:make], model: registration[:model], engine: :ev})
+            new_vehicle = Vehicle.new({vin: registration[:vin_1_10], year: registration[:model_year].to_i, make: registration[:make], model: registration[:model], engine: :ev})
+            #binding.pry
         end
       
         
