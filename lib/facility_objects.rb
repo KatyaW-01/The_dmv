085ce93efdc2fb15a470f372f1f6abe082d9e3b6
Katya Weicht <katya.weicht@gmail.com>
2025-02-15 20:51:25 -0700

    Write and test method for creating new facility objects

--- lib/facility_objects.rb
+++ lib/facility_objects.rb
@@ -1,5 +1,13 @@
+require './lib/facility'
 class FacilityObjects
     def initialize
-        
+
+    end
+
+    def create_facilities(facilities)
+        facilities.map do |facility|
+            new_facility = Facility.new({name: facility[:dmv_office], address: [facility[:address_li],facility[:address__1],facility[:city],facility[:state],facility[:zip]].join(" ") , phone: facility[:phone] })
+            # binding.pry
+        end
     end
 end
--- spec/facility_objects_spec.rb
+++ spec/facility_objects_spec.rb
@@ -10,4 +10,19 @@ RSpec.describe FacilityObjects do
             expect(@facility).to be_a(FacilityObjects)
         end
     end
+    describe "#create facilities" do
+        it 'can create new facilites' do
+            co_facilities = DmvDataService.new.co_dmv_office_locations
+
+            facilities = @facility.create_facilities(co_facilities)
+
+            expect(facilities).to be_an_instance_of(Array)
+
+            expect(facilities[0]).to be_an_instance_of(Facility)
+            expect(facilities[0].name).to eq("DMV Tremont Branch")
+            expect(facilities[0].address).to eq('2855 Tremont Place Suite 118 Denver CO 80205')
+            expect(facilities[0].phone).to eq("(720) 865-4600")
+            expect(facilities[0].services).to eq([])
+        end
+    end
 end
