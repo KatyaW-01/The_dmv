8c11446f35d8679c3dbf156e41e309cfa4b26a16
Katya Weicht <katya.weicht@gmail.com>
2025-02-16 20:39:13 -0700

    Update create_facilities method to fix issue with address values

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -1,28 +1,34 @@
 require './lib/facility'
+require 'pry'
 class FacilityFactory
     
     def initialize
        
     end
 
-    def create_facilities(facilities)
+
+    def create_facilities(facilities,location)
         facilities.map do |facility|
-            new_facility = Facility.new({
-                name: facility[:dmv_office] || facility[:office_name] || facility[:name], 
-                address: [facility[:address_li],facility[:address__1],facility[:city],facility[:state],facility[:zip]].join(" ") || [facility[:street_address_line_1],facility[:city],facility[:state],facility[:zip_code]].join(" ") || [facility[:address1],facility[:city],facility[:state],facility[:zipcode]].join(" "), 
-                phone: facility[:phone] || facility[:public_phone_number]
-                                    })
-        
+            if location == "Colorado"
+                new_facility = Facility.new({
+                    name: facility[:dmv_office],
+                    address: [facility[:address_li],facility[:address__1],facility[:city],facility[:state],facility[:zip]].join(" "),
+                    phone: facility[:phone]
+            })
+            elsif location == "New York"
+                new_facility = Facility.new({
+                    name: facility[:office_name],
+                    address: [facility[:street_address_line_1],facility[:city],facility[:state],facility[:zip_code]].join(" "),
+                    phone: facility[:public_phone_number]
+                })
+            elsif location == "Missouri"
+                new_facility = Facility.new({
+                    name: facility[:name],
+                    address: [facility[:address1],facility[:city],facility[:state],facility[:zipcode]].join(" "),
+                    phone: facility[:phone]
+                })
+            end
         end
     end
 
 end   
-
-#Colorado address key
-#[facility[:address_li],facility[:address__1],facility[:city],facility[:state],facility[:zip]].join(" ")
-
-#New York address key
-#[facility[:street_address_line_1],facility[:city],facility[:state],facility[:zip_code]].join(" ")
-
-#Missouri address key
-# [facility[:address1],facility[:city],facility[:state],facility[:zipcode]].join(" ")
--- spec/facility_factory_spec.rb
+++ spec/facility_factory_spec.rb
@@ -3,6 +3,9 @@ require 'spec_helper'
 RSpec.describe FacilityFactory do
     before(:each) do
         @facility = FacilityFactory.new
+        @co_facilities = DmvDataService.new.co_dmv_office_locations
+        @new_york_facilities = DmvDataService.new.ny_dmv_office_locations
+        @missouri_facilities = DmvDataService.new.mo_dmv_office_locations
     end
 
     describe "#initialize" do
@@ -12,16 +15,14 @@ RSpec.describe FacilityFactory do
     end
     describe "#create facilities" do
         it 'can create new facilites' do
-            co_facilities = DmvDataService.new.co_dmv_office_locations
-
-
-            facilities = @facility.create_facilities(co_facilities)
+            
+            facilities = @facility.create_facilities(@co_facilities,"Colorado")
 
             expect(facilities).to be_an_instance_of(Array)
 
             expect(facilities[0]).to be_an_instance_of(Facility)
             expect(facilities[0].name).to eq("DMV Tremont Branch")
-            #expect(facilities[0].address).to eq('2855 Tremont Place Suite 118 Denver CO 80205')
+            expect(facilities[0].address).to eq('2855 Tremont Place Suite 118 Denver CO 80205')
             expect(facilities[0].phone).to eq("(720) 865-4600")
             expect(facilities[0].services).to eq([])
 
@@ -34,41 +35,36 @@ RSpec.describe FacilityFactory do
 
         it 'can create facilities at multiple locations' do
             #Colorado Offices
-            co_facilities = DmvDataService.new.co_dmv_office_locations
 
-            facilities = @facility.create_facilities(co_facilities)
+            facilities = @facility.create_facilities(@co_facilities,"Colorado")
 
             expect(facilities).to be_an_instance_of(Array)
 
             expect(facilities[0]).to be_an_instance_of(Facility)
             expect(facilities[0].name).to eq("DMV Tremont Branch")
-            #expect(facilities[0].address).to eq('2855 Tremont Place Suite 118 Denver CO 80205')
+            expect(facilities[0].address).to eq('2855 Tremont Place Suite 118 Denver CO 80205')
             expect(facilities[0].phone).to eq("(720) 865-4600")
             expect(facilities[0].services).to eq([])
         
             #New York Offices
-            new_york_facilities = DmvDataService.new.ny_dmv_office_locations
 
-
-            facilities = @facility.create_facilities(new_york_facilities)
+            facilities = @facility.create_facilities(@new_york_facilities, "New York")
 
             expect(facilities[0]).to be_an_instance_of(Facility)
             expect(facilities[0].name).to eq("LAKE PLACID")
-            #expect(facilities[0].address).to eq("2693 MAIN STREET LAKE PLACID NY 12946")
+            expect(facilities[0].address).to eq("2693 MAIN STREET LAKE PLACID NY 12946")
             expect(facilities[0].phone).to eq(nil)
             expect(facilities[0].services).to eq([])
 
             #Missouri Offices
-            missouri_facilities = DmvDataService.new.mo_dmv_office_locations
-
-            facilities = @facility.create_facilities(missouri_facilities)
+            
+            facilities = @facility.create_facilities(@missouri_facilities,"Missouri")
 
             expect(facilities[0]).to be_an_instance_of(Facility)
             expect(facilities[0].name).to eq("Clayton")
-            #expect(facilities[0].address).to eq("147 N Meramec Ave Clayton MO 63105")
+            expect(facilities[0].address).to eq("147 N Meramec Ave Clayton MO 63105")
             expect(facilities[0].phone).to eq("(314) 499-7223")
             expect(facilities[0].services).to eq([])
-
 
         end
     end
